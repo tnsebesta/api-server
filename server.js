7e843125e464826b7fb4e343bbfaa26e60768dd3
Ty Sebesta <tnsebesta1998@gmail.com>
2022-12-15 16:19:58 -0500

    Remove Cred

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 import express from 'express';
 import postgres from 'postgres';
-import {readFile} from 'node:fs/promises';
 
-const sql = postgres("postgres://plant_database_user:SJbAbgdrWTY7DfrU5FggrlqSjb4iGFJ9@dpg-cedo9hsgqg43c91p7n50-a.oregon-postgres.render.com/plant_database?ssl=true");
+
+const sql = postgres(process.env.DATABASE_URL);
 const app = express();
 
 
 app.use(express.json());
 app.use(express.static("./client"));
 
-
-// app.get("/app.js", (req, res) => {
-//     readFile('client/app.js', 'utf-8'). then(text => {
-//         res.set('Content-Type', "application/javascript");
-//         res.send(text);
-//     })
-// });
 
 app.get("/vegetables", (req, res) => {
     sql`SELECT * FROM vegetables`.then((result) => {
